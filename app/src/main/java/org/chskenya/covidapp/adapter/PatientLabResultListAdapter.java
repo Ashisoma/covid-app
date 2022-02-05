a3b3f20daed57fbc82a0cd6cd045b2af45bf527d
Ashisoma <okalionesmus@gmail.com>
2022-02-05 10:28:40 +0300

    sprint

--- app/src/main/java/org/chskenya/covidapp/adapter/PatientLabResultListAdapter.java
+++ app/src/main/java/org/chskenya/covidapp/adapter/PatientLabResultListAdapter.java
@@ -1,4 +1,24 @@
 package org.chskenya.covidapp.adapter;
 
-public class PatientLabResultListAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PatientLabResultListAdapter extends RecyclerView.Adapter<PatientContactListViewAdapter.ViewHolder>{
+    @NonNull
+    @Override
+    public PatientContactListViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PatientContactListViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
