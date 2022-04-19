fd6ba5f81a8348917032bc8fa8e29fe58833643d
Ashisoma <okalionesmus@gmail.com>
2022-04-19 10:10:09 +0300

    final add to reset

---
+++ app/src/main/java/org/chskenya/covidapp/activity/ResetPassord.java
@@ -0,0 +1,16 @@
+package org.chskenya.covidapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.chskenya.covidapp.R;
+
+public class ResetPassord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_passord);
+    }
+}
